e3304974ff21c216dd0e67fe6725cef7bd22b63c
Sir_Boops <admin@boops.me>
2016-09-25 16:12:33 -0600

    Better Photo Queue Checking
    Signed-off-by: Sir_Boops <admin@boops.me>

--- src/main/java/me/boops/functions/queuecheck/QueueCheckPhoto.java
+++ src/main/java/me/boops/functions/queuecheck/QueueCheckPhoto.java
@@ -1,10 +1,13 @@
 package me.boops.functions.queuecheck;
 
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import me.boops.cache.Cache;
-import me.boops.crypto.MD5Sum;
+import me.boops.cache.Config;
 import me.boops.functions.api.APIGetPost;
 import me.boops.functions.api.APIQueueCount;
 import me.boops.functions.api.APIQueueGet;
@@ -13,16 +16,27 @@ import me.boops.logger.Logger;
 public class QueueCheckPhoto {
 
 	public boolean found;
+	OAuthConsumer consumer = new CommonsHttpOAuthConsumer(Config.customer_key,
+			Config.customer_secret);
 
 	public QueueCheckPhoto(Long id, String blog_name) throws Exception {
+
+		// Setup Client
+		consumer.setTokenWithSecret(Config.token, Config.token_secret);
 
 		// Scan The Posts In Queue
 		int scanned = 0;
 
-		// Get Check Post Hash
+		// Get The Post In Question And Define The Postdata String
 		new APIGetPost(id, blog_name);
-		String post_hash = new MD5Sum().hash(Cache.get_post_post.getJSONArray("posts").getJSONObject(0)
-				.getJSONArray("photos").getJSONObject(0).getJSONObject("original_size").getString("url"));
+
+		// Setup The Request
+		String url = (Cache.get_post_post.getJSONArray("posts")
+				.getJSONObject(0).getJSONArray("photos").getJSONObject(0)
+				.getJSONObject("original_size").getString("url"));
+
+		String post_hash = url.split("/")[3];
+		System.out.println(post_hash);
 
 		while (new APIQueueCount().queue_count > scanned && !found) {
 
@@ -34,25 +48,35 @@ public class QueueCheckPhoto {
 			while (sub_runs < posts.length()) {
 
 				// Check If Post Type Is Photo
-				if (((JSONObject) posts.get(sub_runs)).get("type").equals("photo")) {
+				if (((JSONObject) posts.get(sub_runs)).get("type").equals(
+						"photo")) {
 
 					// Calcucate The Post Hash And CHeck it!
-					if (post_hash.equals(new MD5Sum().hash(posts.getJSONObject(sub_runs).getJSONArray("photos")
-							.getJSONObject(0).getJSONObject("original_size").getString("url"))) && !found) {
+					if (post_hash.equals(posts.getJSONObject(sub_runs)
+							.getJSONArray("photos").getJSONObject(0)
+							.getJSONObject("original_size").getString("url")
+							.split("/")[3])
+							&& !found) {
 
 						// Found A Duplicate!
-						new Logger(post_hash + " : "
-								+ new MD5Sum().hash(posts.getJSONObject(sub_runs).getJSONArray("photos")
-										.getJSONObject(0).getJSONObject("original_size").getString("url")),
-								0);
+						new Logger(post_hash
+								+ " : "
+								+ posts.getJSONObject(sub_runs)
+										.getJSONArray("photos")
+										.getJSONObject(0)
+										.getJSONObject("original_size")
+										.getString("url").split("/")[3], 0);
 						found = true;
 					} else {
 
 						// Nope Keep Chacking
-						new Logger(post_hash + " : "
-								+ new MD5Sum().hash(posts.getJSONObject(sub_runs).getJSONArray("photos")
-										.getJSONObject(0).getJSONObject("original_size").getString("url")),
-								0);
+						new Logger(post_hash
+								+ " : "
+								+ posts.getJSONObject(sub_runs)
+										.getJSONArray("photos")
+										.getJSONObject(0)
+										.getJSONObject("original_size")
+										.getString("url").split("/")[3], 0);
 						sub_runs++;
 					}
 				}
@@ -63,5 +87,4 @@ public class QueueCheckPhoto {
 			scanned = (scanned + posts.length());
 		}
 	}
-
 }
