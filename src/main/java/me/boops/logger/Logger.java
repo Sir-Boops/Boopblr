7917dacbb1cbe2a053c7d0b063baf1b6bdeb7658
Sir_Boops <admin@boops.me>
2016-09-28 18:55:10 -0600

    Never assume somthing will work

--- src/main/java/me/boops/logger/Logger.java
+++ src/main/java/me/boops/logger/Logger.java
@@ -8,23 +8,8 @@ public class Logger {
 		
 		
 		// If debug output is enabled display debug output else don't
-		if(debug && Config.debug_output){
-			
-			//0 Is Normal Output
-			if(type == 0){
-				System.out.println("[Boopblr - Message]: " + message);
-			}
-			
-			//1 Is Warning
-			if(type == 1){
-				System.out.println("[Boopblr - Warning]: " + message);
-			}
-			
-			//2 Is Error
-			if(type == 2){
-				System.out.println("[Boopblr - Error]: " + message);
-			}
-			
+		if(debug && !Config.debug_output){
+			return;
 		} else {
 			
 			//0 Is Normal Output
