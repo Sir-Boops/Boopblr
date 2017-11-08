07e474b204249356e12d153ee95212906af9524d
Sir_Boops <admin@boops.me>
2017-11-08 13:15:39 -0700

    Force lower case

--- src/main/java/me/boops/functions/GetTopTags.java
+++ src/main/java/me/boops/functions/GetTopTags.java
@@ -8,8 +8,8 @@ public class GetTopTags {
 		
 		for(int i = 0; i < Cache.allowedTags.size(); i++) {
 			
-			if(!Cache.tagList.contains(Cache.allowedTags.get(i))) {
-				Cache.tagList.add(Cache.allowedTags.get(i));
+			if(!Cache.tagList.contains(Cache.allowedTags.get(i).toLowerCase())) {
+				Cache.tagList.add(Cache.allowedTags.get(i).toLowerCase());
 				Cache.tagCount.add(1);
 			} else {
 				Cache.tagCount.set(Cache.tagList.indexOf(Cache.allowedTags.get(i)), (Cache.tagCount.get(Cache.allowedTags.indexOf(Cache.allowedTags.get(i))) + 1));
