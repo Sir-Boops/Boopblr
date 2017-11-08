7ade4b743181b481da7485e70967ef391aea93ba
Sir_Boops <admin@boops.me>
2017-11-08 13:29:14 -0700

    Logic fix

--- src/main/java/me/boops/functions/GetAllowedAppendTags.java
+++ src/main/java/me/boops/functions/GetAllowedAppendTags.java
@@ -11,14 +11,7 @@ public class GetAllowedAppendTags {
 			boolean ans = false;
 			for(int i2 = 0; i2 < Config.tagAddBlacklist.size(); i2++) {
 				if(Cache.tags.get(i).contains(" ")) {
-					// Run again for the space split
-					for(int i3 = 0; i3 < Cache.tags.get(i).split(" ").length; i3++) {
-						for(int i4 = 0; i4 < Config.tagAddBlacklist.size(); i4++) {
-							if(Config.tagAddBlacklist.get(i4).equalsIgnoreCase(Cache.tags.get(i).split(" ")[i3])) {
-								ans = true;
-							}
-						}
-					}
+					ans = true;
 				} else {
 					if(Config.tagAddBlacklist.get(i2).equalsIgnoreCase(Cache.tags.get(i))) {
 						ans = true;
